089dfe71b191e8d9ee016fbf98a675959016a5b6
Rishabh870 <67893503+Rishabh870@users.noreply.github.com>
2023-03-10 18:28:34 +0530

    Service Added

--- src/Components/Service.jsx
+++ src/Components/Service.jsx
@@ -26,7 +26,47 @@ const services = [
 ];
 
 export const Service = () => {
-  return <section className='section' id='service'></section>;
+  return (
+    <section className='section' id='service'>
+      <div className='container mx-xuto'>
+        <div className='d-flex flex-1 flex-column flex-lg-row mx-5'>
+          <motion.div
+            variants={fadeIn('right', 0.5)}
+            initial='hidden'
+            whileInView='show'
+          >
+            <h2 className='h2 fw-bold mb-4'>What I Do</h2>
+            <h3 className='h3 max-w-455 mb-4'>
+              I'm a Freelancer, Designer, Full Stack Web Developer with 2 year
+              of Experience
+            </h3>
+
+            <button className='btn btn-sm mb-5'>See My Work</button>
+          </motion.div>
+          <div>
+            {services.map((service, index) => {
+              const { name, description, links } = service;
+              return (
+                <motion.div
+                  variants={fadeIn('left', 0.5)}
+                  initial='hidden'
+                  whileInView='show'
+                  className='border-bottom border-white-50 h-146 mb-4 d-flex mx-lg-5 '
+                  key={index}
+                >
+                  <div class='max-w-476'>
+                    <h4 class='fw-bold fs-4 mb-4'>{name}</h4>
+                    <p class='fs-5 lh-base'>{description}</p>
+                  </div>
+                  <div>{links}</div>
+                </motion.div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Service;
