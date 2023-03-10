780a65053672b98e3777fc8d7db5eae9026b01db
Rishabh870 <67893503+Rishabh870@users.noreply.github.com>
2023-03-10 20:54:43 +0530

    Contact Added

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,9 +1,56 @@
 import React from 'react';
-
+import { fadeIn } from '../animation';
+import { motion } from 'framer-motion';
 export const Contact = () => {
   return (
     <section className='section' id='contact'>
-      <div className='container mx-xuto'></div>
+      <div className='container mx-xuto'>
+        <div className='d-flex flex-column flex-lg-row mx-lg-5 align-items-lg-center'>
+          {/* text */}
+          <motion.div
+            variants={fadeIn('up', 0.4)}
+            initial='hidden'
+            whileInView='show'
+            className='flex-grow-1'
+          >
+            <div>
+              <h4 className='text-uppercase text-primary font-medium mb-2 tracking-wide'>
+                Get in touch
+              </h4>
+              <h2
+                style={{ fontSize: '60px' }}
+                className='text-4xl mb-5 mb-lg-2'
+              >
+                Let's work
+                <br /> Together!
+              </h2>
+            </div>
+          </motion.div>
+          <motion.form
+            variants={fadeIn('up', 0.4)}
+            initial='hidden'
+            whileInView='show'
+            className='d-flex flex-grow-1 flex-column gap-y-4 pb-5 px-3 border rounded align-items-start'
+          >
+            <input
+              className='form-control bg-transparent border-0 border-bottom mb-3  my-5 py-3 w-100 transition'
+              type='text'
+              placeholder='Your Name'
+            />
+            <input
+              className='form-control bg-transparent border-0 border-bottom py-3 w-100  transition'
+              type='text'
+              placeholder='Your E-mail'
+            />
+            <textarea
+              placeholder='Your Message'
+              className='textarea form-control bg-transparent border-0 border-bottom py-3 w-100 transition mb-3 my-5'
+            ></textarea>
+
+            <button className='btn btn-lg'>Send Message</button>
+          </motion.form>
+        </div>
+      </div>
     </section>
   );
 };
